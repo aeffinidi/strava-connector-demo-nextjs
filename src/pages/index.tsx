0a29fb48c139a0f96134f88ffe3855f9b6800d34
Atsuhiro Endo <atsuhiro.e@affinidi.com>
2023-03-03 02:33:10 +0100

    feat: add a simple connecto to strava form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-// import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ["latin"] });
+// import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -18,7 +15,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>The journey starts here</h1>
+        <section className="text-gray-600 body-font">
+          <div className="container mx-auto flex flex-col px-5 py-24 justify-center items-center">
+            <Image
+              className="lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded"
+              alt="hero"
+              src="https://cdn.worldvectorlogo.com/logos/strava-2.svg"
+              width={1500}
+              height={1500}
+            />
+            <form className="w-full md:w-2/3 flex flex-col items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl m-6 font-medium text-gray-900">
+                Unlock your digital 6 pack
+              </h1>
+              <div className="flex w-full justify-center items-end">
+                <button className="inline-flex text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Connect now
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
       </main>
     </>
   );
---
+++ src/pages/strava/index.tsx
@@ -0,0 +1,5 @@
+const Strava = () => {
+  return <h1>Strava</h1>;
+};
+
+export default Strava;
